9289159bda20721148926d772e29ef8147cd4bff
Miau <jzh19368@gmail.com>
2022-06-17 21:18:49 -0500

    mod1c

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-var c=0;
+var f = [""];
+var n = [""];
+
 function crear() {
-    var f = ["Banana", "Orange", "Apple", "Mango","Banana", "Orange", "Apple", "Mango"];
-    var N = f.length;
-    var n = ["Anastacia", "Jhon", "Vladimiro", "Julio","Laura", "Albert", "Vanessa", "will"];
-    var p = "";var p1 = "";
-    
-    for (var i = 1; i <= N;i++) {
-      p += "<div id='div1C"+i+"' class='dB' >"+"&nbsp"+i+"<input type='checkbox'  id='I"+i+"' class='ch'>"+f[i-1]+"&nbsp</div>";
-      p1 += "<div id='div2C"+i+"' class='dB1' >"+"&nbsp"+i+"<input type='radio'  id='ID"+i+"' class='ch1'>"+n[i-1]+"&nbsp</div>";
-    }
-    document.getElementsByClassName("propDiv")[0].innerHTML = p;
-    document.getElementsByClassName("propDiv1")[0].innerHTML = p1;
-    var showNextInterval = setInterval(showNext, 500);
+  bR.click(); 
+  f = ["Banana", "Orange", "Apple", "Mango","Strawberry", "Lime", "Pineapple", "Watermelon"];
+  n = ["Anastacia", "Jhon", "Vladimiro", "Julio","Laura", "Albert", "Vanessa", "will"];
+  var p = "";var p1 = "";
+  for (var i = 1; i <= f.length;i++) {
+    p += "<div id='div1C"+i+"' class='dB' >"+"&nbsp"+i+"<input type='checkbox'  id='I"+i+"' class='ch'>"+f[i-1]+"&nbsp</div>";
+    p1 += "<div id='div2C"+i+"' class='dB1' >"+"&nbsp"+i+"<input type='radio'  id='ID"+i+"' class='ch1'>"+n[i-1]+"&nbsp</div>";
+  }
+  document.getElementById("div0").innerHTML = p;//document.getElementsByClassName("propDiv")[0].innerHTML = p;
+  document.getElementById("div1").innerHTML = p1;//document.getElementsByClassName("propDiv1")[0].innerHTML = p1;
+  var showNextInterval = setInterval(showNext, 500);
 }
 
 function showNext() {
-    c++;
-    if(c == 9){
-        c=1;
-    }
-    document.getElementById("I"+c).click();
+  t.value =Number(t.value)+1;
+  if(t.value == 9)t.value = 1;
+  var x = t.value;
+  document.getElementById("I"+x).click();
 
-    if(document.getElementById("I"+c).checked==true){
-      document.getElementById("div1C"+c).style.opacity=0.8;
-     }else{document.getElementById("div1C"+c).style.opacity=1;
-    }
+  if(document.getElementById("I"+x).checked==true){
+    document.getElementById("div1C"+x).style.opacity=0.7;
+   }else{document.getElementById("div1C"+x).style.opacity=1;
+  }
 
-    for (var i = 1; i < 9; i++){
-      if(c==i){document.getElementById("ID"+i).checked=true;document.getElementById("div2C"+i).style.opacity=0.8;
-      }else{document.getElementById("ID"+i).checked=false;document.getElementById("div2C"+i).style.opacity=1;
-      }
+  for (var i = 1; i <= n.length; i++){
+    if(x==i){document.getElementById("ID"+i).checked=true;document.getElementById("div2C"+i).style.opacity=0.7;
+    }else{document.getElementById("ID"+i).checked=false;document.getElementById("div2C"+i).style.opacity=1;
     }
-    document.getElementById("t1").value ="c="+c+ "   id"+c + "    div1C"+c+ "    div2C"+c;
+  }
+  document.getElementById("t1").value = "ID"+x + "    div1C"+x+ "    div2C"+x;
 }
 
 
